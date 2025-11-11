822c88b519b81e630887510e736eeb5e621dfb33
ITM-OscarReyes <oscarreyes291506@correo.itm.edu.co>
2025-11-11 06:51:33 -0500

    Segunda prueba de rutas en GitHub pages

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,62 +1,62 @@
 document.addEventListener("DOMContentLoaded", async () => {
     // Rutas de archivos
     const routes = {
-        /* Partials */
+    /* === Partials === */
         header: {
-            html: "partials/header/header.html",
-            css: "partials/header/header.css",
-            js: "partials/header/header.js",
-            target: "#header"
+        html: 'partials/header/header.html',
+        css: 'partials/header/header.css',
+        js:  'partials/header/header.js',
+        target: '#header'
         },
         content: {
-            html: "../../partials/content/content.html",
-            css: "../../partials/content/content.css",
-            js: "../partials/content/content.js",
-            target: "#content"
+        html: 'partials/content/content.html',
+        css:  'partials/content/content.css',
+        js:   'partials/content/content.js',
+        target: '#content'
         },
         footer: {
-            html: "../partials/footer/footer.html",
-            css: "../partials/footer/footer.css",
-            js: "../partials/footer/footer.js",
-            target: "#footer"
+        html: 'partials/footer/footer.html',
+        css:  'partials/footer/footer.css',
+        js:   'partials/footer/footer.js',
+        target: '#footer'
         },
 
-        /* Sections */
+    /* === Sections === */
         hero: {
-            html: "../sections/hero/hero.html",
-            css: "../sections/hero/hero.css",
-            js: "../sections/hero/hero.js",
-            target: "#hero"
+        html: 'sections/hero/hero.html',
+        css:  'sections/hero/hero.css',
+        js:   'sections/hero/hero.js',
+        target: '#hero'
         },
         about: {
-            html: "../sections/about/about.html",
-            css: "../sections/about/about.css",
-            js: "../sections/about/about.js",
-            target: "#about"
+        html: 'sections/about/about.html',
+        css:  'sections/about/about.css',
+        js:   'sections/about/about.js',
+        target: '#about'
         },
         questions: {
-            html: "../sections/questions/questions.html",
-            css: "../sections/questions/questions.css",
-            js: "../sections/questions/questions.js",
-            target: "#questions"
+        html: 'sections/questions/questions.html',
+        css:  'sections/questions/questions.css',
+        js:   'sections/questions/questions.js',
+        target: '#questions'
         },
         offers: {
-            html: "../sections/offers/offers.html",
-            css: "../sections/offers/offers.css",
-            js: "../sections/offers/offers.js",
-            target: "#offers"
+        html: 'sections/offers/offers.html',
+        css:  'sections/offers/offers.css',
+        js:   'sections/offers/offers.js',
+        target: '#offers'
         },
         stories: {
-            html: "../sections/stories/stories.html",
-            css: "../sections/stories/stories.css",
-            js: "../sections/stories/stories.js",
-            target: "#stories"
+        html: 'sections/stories/stories.html',
+        css:  'sections/stories/stories.css',
+        js:   'sections/stories/stories.js',
+        target: '#stories'
         },
         contact: {
-            html: "../sections/contact/contact.html",
-            css: "../sections/contact/contact.css",
-            js: "../sections/contact/contact.js",
-            target: "#contact"
+        html: 'sections/contact/contact.html',
+        css:  'sections/contact/contact.css',
+        js:   'sections/contact/contact.js',
+        target: '#contact'
         }
     };
 
